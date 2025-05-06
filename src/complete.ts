37b4ff7747f6f3a428e5d5479c7a7438f6b506cc
Krishnan Govindraj <krishnan@typedb.com>
2025-05-06 12:53:30 +0530

    Implement naive variable & label autocompletion

--- src/complete.ts
+++ src/complete.ts
@@ -1,24 +1,89 @@
 import { CompletionContext,  Completion, CompletionResult } from "@codemirror/autocomplete";
 import {syntaxTree} from "@codemirror/language"
-import {SyntaxNode} from "@lezer/common"
+import {SyntaxNode, Tree} from "@lezer/common"
+
+
+function suggestLabel(label: string): Completion {
+    return {
+        label: label,
+        type: "label",
+        apply: label,
+        info: "Label suggestion",
+    }
+}
+
+function suggestVariable(varName: string): Completion {
+    return {
+        label: varName,
+        type: "variable",
+        apply: varName,
+        info: "Variable suggestion",
+    }
+}
+
+function maySuggestKeyword(context: CompletionContext, tree: Tree, atNode: SyntaxNode): Completion[] | null {
+    // TODO:
+    return null;
+}
+
+function collectLabelSuggestions(context: CompletionContext, tree: Tree, atNode: SyntaxNode) : Completion[] {
+    // This is a placeholder function. You will need to implement the logic to collect label suggestions.
+    // For now, we return an empty array.
+    var options : Completion[] = [];
+    tree.iterate({
+        enter: (other: SyntaxNode) => {
+            if (other.name == "LABEL") {
+                let content = context.state.sliceDoc(other.from, other.to);
+                options.push(suggestLabel(content));
+            }
+        }
+    });
+    return options;
+
+}
+
+function collectVariableSuggestions(context: CompletionContext, tree: Tree, atNode: SyntaxNode) : Completion[] {
+    var options : Completion[] = [];
+    tree.iterate({
+        enter: (other: SyntaxNode) => {
+            if (other.name == "VAR") {
+                let content = context.state.sliceDoc(other.from, other.to);
+                options.push(suggestVariable(content));
+            }
+        }
+    });
+    return options;
+}
+
 
 // See: https://codemirror.net/examples/autocompletion/ and maybe the SQL / HTML Example there.
 export function autocompleteTypeQL(context: CompletionContext):  CompletionResult | null {
-    let tree = syntaxTree(context.state);
-    let currentNode: SyntaxNode = tree.resolveInner(context.pos, 0); // https://lezer.codemirror.net/docs/ref/#common.SyntaxNode
+    let tree: Tree = syntaxTree(context.state);
+    let currentNode: SyntaxNode = tree.resolveInner(context.pos, -1); // https://lezer.codemirror.net/docs/ref/#common.SyntaxNode
     // We may have to walk the tree to find the most appropriate node to suggest things based on.
+    let options = null;    
+    if (currentNode.name == "LABEL") {
+        options = collectLabelSuggestions(context, tree, currentNode);
+    } else if (currentNode.name == "VAR") {
+        options = collectVariableSuggestions(context, tree, currentNode);
+    } else {
+        let keywordSuggestions = maySuggestKeyword(context, tree, currentNode);
+        if (keywordSuggestions!= null) {
+            options = keywordSuggestions;
+        }
+    }
 
-    // And once we figure out, we have to create a list of completion objects
-    // It may be worth changing the grammar to be able to do this more easily, rather than replicate the original TypeQL grammar.
-    // https://codemirror.net/docs/ref/#autocomplete.Completion
-    let options = [
-        { label: "(Autocomplete coming soon)", type: "keyword", info: "Feel free to contribute!" }
-    ];
-
-    return {
-        from: context.pos, // Leave this as is
-        options: options,
-        // Docs: "regular expression that tells the extension that, as long as the updated input (the range between the result's from property and the completion point) matches that value, it can continue to use the list of completions."
-        validFor: /^(\w*)?$/ 
+    if (options != null) {
+        // And once we figure out, we have to create a list of completion objects
+        // It may be worth changing the grammar to be able to do this more easily, rather than replicate the original TypeQL grammar.
+        // https://codemirror.net/docs/ref/#autocomplete.Completion
+        return {
+            from: currentNode.from,
+            options: options,
+            // Docs: "regular expression that tells the extension that, as long as the updated input (the range between the result's from property and the completion point) matches that value, it can continue to use the list of completions."
+            validFor: /^(\w+)?$/
+        }
+    } else {
+        return null;
     }
 }
